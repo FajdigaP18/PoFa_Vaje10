e2d982eb2bbd4a0f6db8f42f9d6d4a6e28093380
FajdigaP18 <81242671+FajdigaP18@users.noreply.github.com>
2023-05-31 22:50:31 +0200

    Add files via upload

---
+++ Trki/Form1.Designer.cs
@@ -0,0 +1,139 @@
+﻿namespace Trki
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.pictureBox3 = new System.Windows.Forms.PictureBox();
+            this.pictureBox4 = new System.Windows.Forms.PictureBox();
+            this.kvadratZ = new System.Windows.Forms.PictureBox();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox4)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.kvadratZ)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.BackColor = System.Drawing.Color.Yellow;
+            this.pictureBox2.Location = new System.Drawing.Point(154, 81);
+            this.pictureBox2.Margin = new System.Windows.Forms.Padding(4);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(133, 123);
+            this.pictureBox2.TabIndex = 1;
+            this.pictureBox2.TabStop = false;
+            this.pictureBox2.Tag = "rumen";
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BackColor = System.Drawing.Color.Yellow;
+            this.pictureBox1.Location = new System.Drawing.Point(807, 81);
+            this.pictureBox1.Margin = new System.Windows.Forms.Padding(4);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(133, 123);
+            this.pictureBox1.TabIndex = 2;
+            this.pictureBox1.TabStop = false;
+            this.pictureBox1.Tag = "rumen";
+            // 
+            // pictureBox3
+            // 
+            this.pictureBox3.BackColor = System.Drawing.Color.Yellow;
+            this.pictureBox3.Location = new System.Drawing.Point(807, 371);
+            this.pictureBox3.Margin = new System.Windows.Forms.Padding(4);
+            this.pictureBox3.Name = "pictureBox3";
+            this.pictureBox3.Size = new System.Drawing.Size(133, 123);
+            this.pictureBox3.TabIndex = 3;
+            this.pictureBox3.TabStop = false;
+            this.pictureBox3.Tag = "rumen";
+            // 
+            // pictureBox4
+            // 
+            this.pictureBox4.BackColor = System.Drawing.Color.Yellow;
+            this.pictureBox4.Location = new System.Drawing.Point(154, 371);
+            this.pictureBox4.Margin = new System.Windows.Forms.Padding(4);
+            this.pictureBox4.Name = "pictureBox4";
+            this.pictureBox4.Size = new System.Drawing.Size(133, 123);
+            this.pictureBox4.TabIndex = 4;
+            this.pictureBox4.TabStop = false;
+            this.pictureBox4.Tag = "rumen";
+            // 
+            // kvadratZ
+            // 
+            this.kvadratZ.BackColor = System.Drawing.Color.Green;
+            this.kvadratZ.Location = new System.Drawing.Point(487, 227);
+            this.kvadratZ.Margin = new System.Windows.Forms.Padding(4);
+            this.kvadratZ.Name = "kvadratZ";
+            this.kvadratZ.Size = new System.Drawing.Size(133, 123);
+            this.kvadratZ.TabIndex = 5;
+            this.kvadratZ.TabStop = false;
+            this.kvadratZ.Tag = "";
+            // 
+            // timer1
+            // 
+            this.timer1.Enabled = true;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1109, 596);
+            this.Controls.Add(this.kvadratZ);
+            this.Controls.Add(this.pictureBox4);
+            this.Controls.Add(this.pictureBox3);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.pictureBox2);
+            this.Name = "Form1";
+            this.Tag = "rumen";
+            this.Text = "Trki";
+            this.KeyDown += new System.Windows.Forms.KeyEventHandler(this.pritisnemoTipko);
+            this.KeyUp += new System.Windows.Forms.KeyEventHandler(this.spustimoTipko);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox4)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.kvadratZ)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox2;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.PictureBox pictureBox3;
+        private System.Windows.Forms.PictureBox pictureBox4;
+        private System.Windows.Forms.PictureBox kvadratZ;
+        private System.Windows.Forms.Timer timer1;
+    }
+}
+
---
+++ Trki/Form1.cs
@@ -0,0 +1,126 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Trki
+{
+    public partial class Form1 : Form
+    {
+        bool tipkaGor, tipkaDol, tipkaLevo, tipkaDesno;
+        int premik = 10;
+
+        private void spustimoTipko(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.Up)
+            {
+                tipkaGor = false;
+            }
+
+            if (e.KeyCode == Keys.Down)
+            {
+                tipkaDol = false;
+            }
+
+            if (e.KeyCode == Keys.Left)
+            {
+                tipkaLevo = false;
+            }
+
+            if (e.KeyCode == Keys.Right)
+            {
+                tipkaDesno = false;
+            }
+        }
+
+        private void pritisnemoTipko(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.Up)
+            {
+                tipkaGor = true;
+            }
+
+            if (e.KeyCode == Keys.Down)
+            {
+                tipkaDol = true;
+            }
+
+            if (e.KeyCode == Keys.Left)
+            {
+                tipkaLevo = true;
+            }
+
+            if (e.KeyCode == Keys.Right)
+            {
+                tipkaDesno = true;
+            }
+
+        }
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        /// <summary>
+        /// preverimo ali se zelen kvadrat prekriva s katerim rumenim
+        /// </summary>
+        private void PreverimoPrekrivanje()
+        {
+            // pregled nad prekrivanjem
+            foreach (Control element in Controls)
+            {
+                if (element is PictureBox && (string)element.Tag == "rumen")
+                {
+
+                    // ob prekrivanju kvadratov, barvo rumenega nastavimo na rdečo
+                    if (kvadratZ.Bounds.IntersectsWith(element.Bounds))
+                    {
+                        element.BackColor = Color.Red;
+                    }
+
+                    // barva rumenegakvadrata se ne spremeni
+                    else
+                    {
+                        element.BackColor = Color.Yellow;
+                    }
+                }
+            }
+        }
+
+        /// <summary>
+        /// Premikanje Zelenega kvadrata
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            if (tipkaDol == true && kvadratZ.Bottom < this.Height)
+            {
+                kvadratZ.Top += premik;
+            }
+
+            if (tipkaLevo == true && kvadratZ.Left > 0)
+            {
+                kvadratZ.Left -= premik;
+            }
+
+            if (tipkaDesno == true && kvadratZ.Right < this.Width)
+            {
+                kvadratZ.Left += premik;
+            }
+
+            if (tipkaGor == true && kvadratZ.Top > 0)
+            {
+                kvadratZ.Top -= premik;
+            }
+            PreverimoPrekrivanje();
+
+        }
+    }
+}
